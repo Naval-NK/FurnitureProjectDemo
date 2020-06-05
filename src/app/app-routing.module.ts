c91561cdcbf6f5a4fbb7da8648f99a0a711e1ffd
Naval-NK <naval_kulkarni@yahoo.com>
2020-06-05 19:22:13 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   // FULL TELL IF FULL URL IS EMPTY GO TO HOMECOMPONENT
   { path:'',redirectTo : '/home',pathMatch : 'full' },
-  { path:'https://tablewoodfs.netlify.app',redirectTo : 'https://tablewoodfs.netlify.app/home',pathMatch : 'full' },
   { path:'home' , component :WinComponent },
   { path:'living', component  : LivingRoomComponent},
   { path:'kitchen',  component :  KitchenComponent},
